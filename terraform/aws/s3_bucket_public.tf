c8137ec58b626365350fa6e8b224334b56fdef8f
Dudu Nahum <dudu.nahum@ermetic.com>
2023-01-05 10:43:15 +0200

    Newly created repo, copied from the Github repo

---
+++ terraform/aws/s3_bucket_public.tf
@@ -0,0 +1,7 @@
+resource "aws_s3_bucket_public_access_block" "positive2" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls   = true
+  block_public_policy = false
+  ignore_public_acls  = false
+}
